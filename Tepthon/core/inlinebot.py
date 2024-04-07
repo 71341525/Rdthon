33ded6dd3a85c746c95c4d8d1b77741018648e88
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-04-07 22:31:12 +0300

    Update inlinebot.py

--- Tepthon/core/inlinebot.py
+++ Tepthon/core/inlinebot.py
@@ -314,10 +314,10 @@ async def inline_handler(event):  # sourcery no-metrics
     else:
         buttons = [
             (
-                Button.url("قنـاة السـورس", "https://t.me/Tepthon"),
+                Button.url("قنـاة السـورس", "https://t.me/A0Rll"),
                 Button.url(
                     "مطـور السـورس",
-                    "https://t.me/A_D_P",
+                    "https://t.me/r_32i",
                 ),
             )
         ]
@@ -326,14 +326,14 @@ async def inline_handler(event):  # sourcery no-metrics
             url=ZEDLOGO, size=0, mime_type="image/jpeg", attributes=[]
         )
         text, msg_entities = await event.client._parse_message_text(
-            "𝗗𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗼𝘄𝗻 𝗧𝗘𝗣𝗧𝗛𝗢𝗡.", "md"
+            "𝗗𝗲𝗽𝗹𝗼𝘆 𝘆𝗼𝘂𝗿 𝗼𝘄𝗻 𝗥𝗱𝘁𝗵𝗼𝗻.", "md"
         )
         result = types.InputBotInlineResult(
             id=str(uuid4()),
             type="photo",
-            title="𝗧𝗘𝗣𝗧𝗛𝗢𝗡 𓅛",
+            title="𝗥𝗱𝘁𝗵𝗼𝗻 𓅛",
             description="روابـط التنصـيب",
-            url="https://t.me/Tepthon",
+            url="https://t.me/A0Rll",
             thumb=photo,
             content=photo,
             send_message=types.InputBotInlineMessageMediaAuto(
