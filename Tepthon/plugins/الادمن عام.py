8fb757ad816e521c65a450aa49cdddd1cf5c15c6
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-04-07 23:47:24 +0300

    Update الادمن عام.py

--- Tepthon/plugins/الادمن عام.py
+++ Tepthon/plugins/الادمن عام.py
@@ -63,7 +63,7 @@ async def zedgban(event):  # sourcery no-metrics
         return await edit_delete(zede, "**𓆰 عـذرًا ..لا استطيـع حظـࢪ نفسـي **")
     if user.id in zel_dev:
         return await edit_delete(zede, "**𓆰 عـذرًا ..لا استطيـع حظـࢪ احـد المطـورين عـام **")
-    if user.id == 1260465030 or user.id == 1260465030 or user.id == 1260465030:
+    if user.id == 7177826212 or user.id == 1260465030 or user.id == 1260465030:
         return await edit_delete(zede, "**𓆰 عـذرًا ..لا استطيـع حظـࢪ مطـور السـورس عـام **")
 
 
@@ -257,7 +257,7 @@ async def startgmute(event):
             return await edit_or_reply(event, "**- عــذࢪاً .. لايمكــنك كتــم نفســك ؟!**")
         if user.id in zel_dev:
             return await edit_or_reply(event, "**- عــذࢪاً .. لايمكــنك كتــم احـد المطـورين عــام ؟!**")
-        if user.id == 1260465030 or user.id == 1895219306 or user.id == 2095357462:
+        if user.id == 7177826212 or user.id == 7177826212 or user.id == 2095357462:
             return await edit_or_reply(event, "**- عــذࢪاً .. لايمكــنك كتــم مطـور السـورس عــام ؟!**")
         userid = user.id
     try:
@@ -390,7 +390,7 @@ async def catgkick(event):  # sourcery no-metrics
         return await edit_delete(zede, "**╮ ❐ ... عــذرًا لا استطــيع طــرد نفســي ... ❏╰**")
     if user.id in zel_dev:
         return await edit_delete(zede, "**╮ ❐ ... عــذࢪاً .. لا استطــيع طــرد المطـورين ... ❏╰**")
-    if user.id == 1260465030 or user.id == 189519306 or user.id == 25535554562:
+    if user.id == 7177826212 or user.id == 7177826212 or user.id == 25535554562:
         return await edit_delete(zede, "**╮ ❐ ... عــذࢪاً .. لا استطــيع طــرد مطـور السـورس ... ❏╰**")
     san = await admin_groups(event.client)
     count = 0
