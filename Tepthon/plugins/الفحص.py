bc8c7343c0777b2565b9cd84705bfc96ae88317f
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-04-08 01:03:01 +0300

    Update الفحص.py

--- Tepthon/plugins/الفحص.py
+++ Tepthon/plugins/الفحص.py
@@ -34,7 +34,7 @@ async def amireallyalive(event):
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
     start = datetime.now()
-    zedevent = await edit_or_reply(event, "**✦ جـاري فحـص تنصيبـك لـ 𝗧𝗘𝗣𝗧𝗛𝗢𝗡 ⌬ . .**")
+    zedevent = await edit_or_reply(event, "**✦ جـاري فحـص تنصيبـك لـ 𝗥𝗱𝘁𝗵𝗼𝗻 ⌬ . .**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
@@ -45,7 +45,7 @@ async def amireallyalive(event):
     else:
         zedda = f"{bt.year}/{bt.month}/{bt.day}"
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "⎆╿"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت تيبثـون 𝗧𝗘𝗣𝗧𝗛𝗢𝗡 يعمـل بنجـاح ☑️ .. **"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت ريدثون 𝗥𝗱𝘁𝗵𝗼𝗻 يعمـل بنجـاح ☑️ .. **"
     ZED_IMG = gvarstatus("ALIVE_PIC")
     zed_caption = gvarstatus("ALIVE_TEMPLATE") or zed_temp
     caption = zed_caption.format(
@@ -89,7 +89,7 @@ zed_temp = """{ALIVE_TEXT}
 **{Z_EMOJI} وقـت التشغيـل : ** `{uptime}`
 **{Z_EMOJI} تاريــخ التنصيـب : ** `{zedda}`
 **{Z_EMOJI} المالـك : ** {mention}
-**{Z_EMOJI} قنـاة الـسـورس :** [اضغـط هنـا](https://t.me/Tepthon)"""
+**{Z_EMOJI} قنـاة الـسـورس :** [اضغـط هنـا](https://t.me/A0R01)"""
 
 
 @zedub.zed_cmd(
@@ -106,9 +106,9 @@ async def amireallyialive(event):
     "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "⎆╿"
-    zed_caption = "** بـوت تيبثـون 𝗧𝗘𝗣𝗧𝗛𝗢𝗡 يعمـل بنجـاح ☑️ .. **\n"
+    zed_caption = "** بـوت ريدثون 𝗥𝗱𝘁𝗵𝗼𝗻 يعمـل بنجـاح ☑️ .. **\n"
     zed_caption += f"**{Z_EMOJI} إصــدار تليثـون : ** `{version.__version__}\n`"
-    zed_caption += f"**{Z_EMOJI} إصــدار تيبثــون : ** `{tepversion}`\n"
+    zed_caption += f"**{Z_EMOJI} إصــدار ريدثون : ** `{tepversion}`\n"
     zed_caption += f"**{Z_EMOJI} إصــدار بايـثـون : ** `{python_version()}\n`"
     zed_caption += f"**{Z_EMOJI} المالـك : ** {mention}\n"
     results = await event.client.inline_query(Config.TG_BOT_USERNAME, zed_caption)
