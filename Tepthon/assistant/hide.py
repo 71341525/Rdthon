425012e95d3827e4b83cdeafef0b427945a9124d
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-04-07 22:06:11 +0300

    Update hide.py

--- Tepthon/assistant/hide.py
+++ Tepthon/assistant/hide.py
@@ -15,7 +15,7 @@ async def on_plug_in_callback_query_handler(event):
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات تـيـبثون"
+            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات ردثـون"
     else:
-        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات تــيـبثون"
+        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجودة في سيـرفرات ردثـون"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
